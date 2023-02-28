cdf4e86b579671a3d9744b6bc84e46f31cb9882b
Łukasz <lukasz13102001@o2.pl>
2023-02-28 12:03:54 +0100

    production

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import NewQuiz from './components/NewQuiz';
 import Quiz from './components/Quiz';
 
